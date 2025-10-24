2ae72caa439e5e58cb64f5b069d0e4ece36549c2
maliandri <maliandri@outlook.com>
2025-10-24 18:44:18 -0300

    holaaa

--- src/components/aichatbot.jsx
+++ src/components/aichatbot.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import './AIChatbot.css';
+import './aichatbot.css';
 
 function AIChatBot() {
   const [isOpen, setIsOpen] = useState(false);
